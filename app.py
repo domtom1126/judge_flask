1f146367eeb0dc9a164a8a667f2b005f1cb55588
domtom1126 <domtom1126@gmail.com>
2020-06-23 06:58:32 -0700

    Functionality working, need to fix total cases for judge, double in html.

--- app.py
+++ app.py
@@ -10,13 +10,15 @@ def home():
    return  render_template('home.html')
 
 @app.route('/judge_return', methods=['POST', 'GET'])
-def judge_return():
+def judge_return(judge=None, statute=None):
       # index.html, name=judge_name 
    judge = request.form['judge_name']
+   statute = request.form['statute']
    con = sql.connect('judge.db')
    c = con.cursor()
-   c.execute("SELECT * FROM judge_statute_total WHERE judge = (?)", (judge))
-   con.commit()
-   display = c.fetchall()
-   return render_template('judge_return.html', display=display)
+   total_cases_for_judge =  c.execute("SELECT judge_total FROM judge_statute_total WHERE judge=? AND statute=?",(judge,statute))
+   display_judge_total = total_cases_for_judge.fetchall()
+   total_cases_for_database = c.execute("SELECT stat_totals FROM judge_statute_total WHERE judge=? AND statute=?",(judge,statute))
+   display_total_cases_for_database = total_cases_for_database.fetchone()   
+   return render_template('judge_return.html',display_judge_total=display_judge_total, display_total_cases_for_database=display_total_cases_for_database,judge=judge, statute=statute)
     
